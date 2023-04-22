6ee5d6b7c6abca982a7ab4ddff054ff902bbfe84
gamertop13lol <gamertop13lol@gmail.com>
2023-04-22 22:06:46 +0100

    Added savcother, delc and allc

--- src/main/java/com/Gamer_top_13/skyqol/dosth/CoordUtils.java
+++ src/main/java/com/Gamer_top_13/skyqol/dosth/CoordUtils.java
@@ -11,12 +11,14 @@ public class CoordUtils {
     static List savedcoords = new ArrayList();
     public static void savc(String[] args) {
         if (args.length >= 3) {
+            Gui gui = Minecraft.getInstance().gui;
             String name = args[2];
             String x = String.valueOf(Minecraft.getInstance().player.getBlockX());
             String y = String.valueOf(Minecraft.getInstance().player.getBlockY());
             String z = String.valueOf(Minecraft.getInstance().player.getBlockZ());
             List<String> putcoords = Arrays.asList(name, x, y, z);
             savedcoords.add(putcoords);
+            gui.getChat().addMessage(Component.translatable("skyqol.command.savc.confirm", name, x, y, z));
         } else {
             Minecraft.getInstance().gui.getChat().addMessage(Component.translatable("skyqol.command.savc.nea"));
         }
@@ -36,6 +38,50 @@ public class CoordUtils {
             }
         } else {
             Minecraft.getInstance().gui.getChat().addMessage(Component.translatable("skyqol.command.getc.nea"));
+        }
+    }
+    public static void savcother(String[] args) {
+        if (args.length >= 6) {
+            Gui gui = Minecraft.getInstance().gui;
+            String name = args[2];
+            String x = args[3];
+            String y = args[4];
+            String z = args[5];
+            List<String> putcoords = Arrays.asList(name, x, y, z);
+            savedcoords.add(putcoords);
+            gui.getChat().addMessage(Component.translatable("skyqol.command.savcother.confirm", name, x, y, z));
+        } else {
+            Minecraft.getInstance().gui.getChat().addMessage(Component.translatable("skyqol.command.savcother.nea"));
+        }
+    }
+    public static void delc(String[] args) {
+        if (args.length >= 3) {
+            for (int i=0;i<savedcoords.size();i++) {
+                List a = (List) savedcoords.get(i);
+                if (a.get(0).equals(args[2])) {
+                    Gui gui = Minecraft.getInstance().gui;
+                    String name = (String) a.get(0);
+                    String x = (String) a.get(1);
+                    String y = (String) a.get(2);
+                    String z = (String) a.get(3);
+                    savedcoords.remove(i);
+                    gui.getChat().addMessage(Component.translatable("skyqol.command.delc.confirm", name, x, y, z));
+                }
+            }
+        } else {
+            Minecraft.getInstance().gui.getChat().addMessage(Component.translatable("skyqol.command.delc.nea"));
+        }
+    }
+    public static void allc(String[] args) {
+        Gui gui = Minecraft.getInstance().gui;
+        gui.getChat().addMessage(Component.translatable("skyqol.command.allc.title"));
+        for (int i=0;i<savedcoords.size();i++) {
+            List a = (List) savedcoords.get(i);
+            String name = (String) a.get(0);
+            String x = (String) a.get(1);
+            String y = (String) a.get(2);
+            String z = (String) a.get(3);
+            gui.getChat().addMessage(Component.translatable("skyqol.command.allc.out", name, x, y, z));
         }
     }
 }
--- src/main/java/com/Gamer_top_13/skyqol/dosth/SkyQOLCommand.java
+++ src/main/java/com/Gamer_top_13/skyqol/dosth/SkyQOLCommand.java
@@ -5,17 +5,19 @@ import net.minecraft.client.gui.Gui;
 import net.minecraft.network.chat.Component;
 import com.Gamer_top_13.skyqol.dosth.CoordUtils;
 
-import static com.Gamer_top_13.skyqol.dosth.CoordUtils.getc;
-import static com.Gamer_top_13.skyqol.dosth.CoordUtils.savc;
+import static com.Gamer_top_13.skyqol.dosth.CoordUtils.*;
 
 public class SkyQOLCommand {
     public static void splitAndInterpret(String text) {
         String[] args = text.split(" ");
         if (args.length >= 2) {
             switch (args[1]) {
-                case "help" -> help();
+                case "help" -> help(args);
                 case "savc" -> savc(args);
                 case "getc" -> getc(args);
+                case "savcother" -> savcother(args);
+                case "delc" -> delc(args);
+                case "allc" -> allc(args);
             }
         } else {
             Gui gui = Minecraft.getInstance().gui;
@@ -23,10 +25,21 @@ public class SkyQOLCommand {
         }
     }
 
-    private static void help() {
+    private static void help(String[] args) {
         Gui gui = Minecraft.getInstance().gui;
-        gui.getChat().addMessage(Component.translatable("skyqol.help.title"));
-        gui.getChat().addMessage(Component.translatable("skyqol.help.savc"));
-        gui.getChat().addMessage(Component.translatable("skyqol.help.getc"));
+        if (args.length >= 3) {
+            switch (args[3]) {
+                case "savc" -> gui.getChat().addMessage(Component.translatable("skyqol.help.savc"));
+                case "getc" -> gui.getChat().addMessage(Component.translatable("skyqol.help.getc"));
+                case "savcother" -> gui.getChat().addMessage(Component.translatable("skyqol.help.savcother"));
+                case "delc" -> gui.getChat().addMessage(Component.translatable("skyqol.help.delc"));
+            }
+        } else {
+            gui.getChat().addMessage(Component.translatable("skyqol.help.title"));
+            gui.getChat().addMessage(Component.translatable("skyqol.help.savc"));
+            gui.getChat().addMessage(Component.translatable("skyqol.help.getc"));
+            gui.getChat().addMessage(Component.translatable("skyqol.help.savcother"));
+            gui.getChat().addMessage(Component.translatable("skyqol.help.delc"));
+        }
     }
 }
